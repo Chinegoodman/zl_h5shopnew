8d5950dc80369b41258931d92899e856b2c88577
庞世锋 <pangshifeng0539@126.com>
2020-05-28 15:45:46 +0800

    seo testing

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,8 @@ const  { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');  //webpa
 
 // prerender-spa-plugin插件实现添加预渲染  SEO相关
 var path = require('path')
-var PrerenderSpaPlugin = require('prerender-spa-plugin')
+var PrerenderSPAPlugin = require('prerender-spa-plugin')
+var Renderer = PrerenderSPAPlugin.PuppeteerRenderer
 
 let current_outputDir = '';
 if (process.env.VUE_APP_ENV == 'uat') {
@@ -53,14 +54,20 @@ module.exports = {
     //其他配置....
     configureWebpack: {
         plugins: [
-            new PrerenderSpaPlugin(
-                // 编译后的html需要存放的路径
-                path.join(__dirname, `../${current_outputDir}`),
-                // 列出哪些路由需要预渲染
-                ['/', '/shop/productdetails', '/contact']
-            ),
+            new PrerenderSPAPlugin({
+                staticDir: path.join(__dirname, `../${current_outputDir}`),
+                routes: ['/', '/login'],
+
+                renderer: new Renderer({
+                    inject: {
+                        foo: 'bar'
+                    },
+                    headless: true,
+                    renderAfterDocumentEvent: 'render-event'
+                })
+            })
             //TODO: 灰度及生产环境的 包 不执行此插件 【防止jenkins打包不成功】
-            new BundleAnalyzerPlugin(), //webpack-bundle-analyzer 分析打包体积 暂时展示
+            // new BundleAnalyzerPlugin(), //webpack-bundle-analyzer 分析打包体积 暂时展示
         ]
     }
     // 这样写会报错。放到上边的 configureWebpack 里就可以了
