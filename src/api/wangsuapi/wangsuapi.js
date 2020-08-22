a7e02ed1aa9cc98c7b1c6d87dbf427bf326ccba7
lijiaxiong <lijiaxiong@17biyi.com>
2020-08-22 14:38:01 +0800

    注意http_json

--- src/api/wangsuapi/wangsuapi.js
+++ src/api/wangsuapi/wangsuapi.js
@@ -4,7 +4,7 @@
 
 import base from '../base'; // 导入接口域名列表
 import axios from '@/request/http'; // 导入http中创建的axios实例
-import axios_json from '@/request/http_json'; // 导入http中创建的axios实例
+// import axios_json from '@/request/http_json'; // 导入http中创建的axios实例
 import qs from 'qs'; // 根据需求是否导入qs模块
 
 const wangsuapi = {
